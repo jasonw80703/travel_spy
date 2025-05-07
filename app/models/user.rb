c6fedd487661dd83f0714675e029c1484c469de1
Jason Wang <jasonprime80704@gmail.com>
2025-05-07 12:45:35 -0700

    chore: add annotaterb gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  password   :string
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
-  
+  def name
+    
+  end
 end
